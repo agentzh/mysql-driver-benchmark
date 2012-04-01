547c9f2e2d6f81b8c96a5ab7ac85e38ace7aae7f
agentzh (章亦春) <agentzh@gmail.com>
2012-04-01 22:57:26 +0800

    now we plot charts for memory footprint (VSZ/RSS) and CPU usage as well.

--- plot-slow.r
+++ plot-slow.r
@@ -1,20 +1,48 @@
 slow <- read.csv("slow.csv")
 
-png("drizzle-slow-micro-lowess.png", pointsize = 15)
+png("rps-drizzle-slow-micro-lowess.png", pointsize = 15)
 
 plot(slow$con, slow$rps, xlab="concurrency", ylab="requests per second",
     main="ngx_drizzle/ngx_rds_json on Amazon EC2 Micro",
-    sub="MySQL Query \"select sleep(1)\" (LOWESS Chart)")
+    sub="MySQL Query \"select sleep(1)\" (RPS LOWESS Chart)")
 
 lines(lowess(slow$con, slow$rps), col="blue")
 
 dev.off()
 
-png("drizzle-slow-micro.png", pointsize = 15)
+png("rps-drizzle-slow-micro.png", pointsize = 15)
 
 plot(slow$con, slow$rps, xlab="concurrency", ylab="requests per second",
     type="l", main="ngx_drizzle/ngx_rds_json on Amazon EC2 Micro",
-    sub="MySQL Query \"select sleep(1)\" (Line Chart)")
+    sub="MySQL Query \"select sleep(1)\" (RPS Line Chart)")
+
+dev.off()
+
+png("vsz-drizzle-slow-micro.png", pointsize = 15)
+
+plot(slow$con, slow$nginx_vsz, xlab="concurrency", ylab="Nginx Worker VSZ (in MBytes)",
+    type="l", main="ngx_drizzle/ngx_rds_json on Amazon EC2 Micro",
+    sub="MySQL Query \"select sleep(1)\" (Nginx VSZ Line Chart)")
+
+dev.off()
+
+png("rss-drizzle-slow-micro.png", pointsize = 15)
+
+plot(slow$con, slow$nginx_rss, xlab="concurrency", ylab="Nginx Worker RSS (in MBytes)",
+    type="l", main="ngx_drizzle/ngx_rds_json on Amazon EC2 Micro",
+    sub="MySQL Query \"select sleep(1)\" (Nginx RSS Line Chart)")
+
+#lines(slow$con, slow$nginx_rss, col="red")
+
+dev.off()
+
+png("cpu-drizzle-slow-micro.png", pointsize = 15)
+
+plot(slow$con, slow$nginx_cpu, xlab="concurrency", ylab="Nginx Worker CPU Usage (in %)",
+    type="l", main="ngx_drizzle/ngx_rds_json on Amazon EC2 Micro",
+    sub="MySQL Query \"select sleep(1)\" (Nginx CPU Line Chart)")
+
+#lines(slow$con, slow$nginx_rss, col="red")
 
 dev.off()
 
