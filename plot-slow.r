72f8a708cacda6fff2b7bc1387949ff46bf6d2b7
agentzh (章亦春) <agentzh@gmail.com>
2012-04-02 14:03:14 +0800

    added two more charts: nginx throughput chart and nginx average response time chart.

--- plot-slow.r
+++ plot-slow.r
@@ -1,6 +1,6 @@
 slow <- read.csv("slow.csv")
 
-png("rps-lowess-nginx.png", pointsize = 15)
+png("rps-lowess-nginx.png", pointsize = 16)
 
 plot(slow$con, slow$rps, xlab="concurrency", ylab="requests per second",
     main="Nginx Requests Per Second (LOWESS Chart)")
@@ -9,21 +9,36 @@ lines(lowess(slow$con, slow$rps), col="blue")
 
 dev.off()
 
-png("rps-nginx.png", pointsize = 15)
+png("rps-nginx.png", pointsize = 16)
 
 plot(slow$con, slow$rps, xlab="concurrency", ylab="requests per second",
     type="l", main="Nginx Requests Per Second")
 
 dev.off()
 
-png("vsz-nginx.png", pointsize = 15)
+png("time-nginx.png", pointsize = 16)
+
+plot(slow$con, slow$time, xlab="concurrency", ylab="time (millisecons)",
+    type="l", main="Nginx Average Response Time")
+
+dev.off()
+
+png("throughput-nginx.png", pointsize = 16)
+
+plot(slow$con, slow$throughput, xlab="concurrency", ylab="Throughput (KBytes per second)",
+    type="l", main="Nginx Throughput")
+
+dev.off()
+
+
+png("vsz-nginx.png", pointsize = 16)
 
 plot(slow$con, slow$nginx_vsz, xlab="concurrency", ylab="VSZ (MBytes)",
     type="l", main="Nginx Worker Memory Footprint (VSZ)")
 
 dev.off()
 
-png("rss-nginx.png", pointsize = 15)
+png("rss-nginx.png", pointsize = 16)
 
 plot(slow$con, slow$nginx_rss, xlab="concurrency", ylab="RSS (MBytes)",
     type="l", main="Nginx Worker Memory Footprint (RSS)")
@@ -32,35 +47,35 @@ plot(slow$con, slow$nginx_rss, xlab="concurrency", ylab="RSS (MBytes)",
 
 dev.off()
 
-png("cpu-nginx.png", pointsize = 15)
+png("cpu-nginx.png", pointsize = 16)
 
 plot(slow$con, slow$nginx_cpu, xlab="concurrency", ylab="CPU (%)",
     type="l", main="Nginx Worker CPU Usage")
 
 dev.off()
 
-png("cpu-mysql.png", pointsize = 15)
+png("cpu-mysql.png", pointsize = 16)
 
 plot(slow$con, slow$mysql_cpu, xlab="concurrency", ylab="CPU (%)",
     type="l", main="MySQL CPU Usage")
 
 dev.off()
 
-png("rss-mysql.png", pointsize = 15)
+png("rss-mysql.png", pointsize = 16)
 
 plot(slow$con, slow$mysql_rss, xlab="concurrency", ylab="RSS (MBytes)",
     type="l", main="MySQL Memory Footprint (RSS)")
 
 dev.off()
 
-png("vsz-mysql.png", pointsize = 15)
+png("vsz-mysql.png", pointsize = 16)
 
 plot(slow$con, slow$mysql_vsz, xlab="concurrency", ylab="VSZ (MBytes)",
     type="l", main="MySQL Memory Footprint (VSZ)")
 
 dev.off()
 
-png("threads-mysql.png", pointsize = 15)
+png("threads-mysql.png", pointsize = 16)
 
 plot(slow$con, slow$mysql_threads, xlab="concurrency", ylab="Threads",
     type="l", main="MySQL Thread Count")
