5efc10c314c036ed73ef185dab8c006a4514daf3
agentzh (章亦春) <agentzh@gmail.com>
2012-04-05 21:58:21 +0800

    fixed a typo.

--- php-fpm/slow.php
+++ php-fpm/slow.php
@@ -17,7 +17,7 @@ mysql_select_db('world') or die('Could not select database');
 
 // Performing SQL query
 // $query = 'SELECT sleep(1)';
-$query = "select * from City order by ID limit 1"
+$query = "select * from City order by ID limit 1";
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
 $lines = array();
