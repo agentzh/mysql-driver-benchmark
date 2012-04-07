7af368a636c0bec0fdca9d17e7f6a94f1a983309
agentzh (章亦春) <agentzh@gmail.com>
2012-04-07 10:31:55 +0800

    more updates in the php scripts.

--- php-fpm/slow.php
+++ php-fpm/slow.php
@@ -1,10 +1,10 @@
 <?php
 
 $nodes = array(
-    "10.211.162.185",
-    "10.211.93.139",
-    "10.192.15.221",
-    "10.210.187.58"
+    "mysql01",
+    "mysql02",
+    "mysql03",
+    "mysql04"
 );
 
 $i = rand(0, count($nodes) - 1);
@@ -16,8 +16,8 @@ $link = mysql_connect($nodes[$i], 'ngx_test', 'ngx_test')
 mysql_select_db('world') or die('Could not select database');
 
 // Performing SQL query
-// $query = 'SELECT sleep(1)';
-$query = "select * from City order by ID limit 1";
+$query = 'SELECT sleep(1)';
+// $query = "select * from City order by ID limit 1";
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
 $lines = array();
